1c240606e02f98c60f6715cc56766260ca2ce09b
David <davidbenjamin0685@gmail.com>
2020-07-29 21:30:17 -0400

    studying for logic

---
+++ introfor/forloop3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+for x in farms:
+    print("The sheep is:" + x)
+print("Aaaaaaaaand the loop is donesies!")
+
+# Can't figure this one out yet. Trying to make a list showing animals in each farm. Help appreciated!
+
